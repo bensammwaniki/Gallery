7beb7e1213d928a16b3fad510b40645daedf94ea
bensammwaniki <bensammwaniki@gmail.com>
2021-10-11 05:10:06 +0300

    configuring media roots

--- gallery/settings.py
+++ gallery/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 # media urls 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
