aa1b776c130d0754ebec67da104b97fa2b412f9d
bensammwaniki <bensammwaniki@gmail.com>
2021-10-11 20:19:02 +0300

    deploy

--- gallery/settings.py
+++ gallery/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 MODE=config("MODE", default="dev")
+
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # development
