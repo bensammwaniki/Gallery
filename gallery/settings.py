43e7a882311290c087557478631ca3944de1c1e8
bensammwaniki <bensammwaniki@gmail.com>
2021-10-11 20:45:56 +0300

    deploy

--- gallery/settings.py
+++ gallery/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='django-insecure-xx8z#nn3t&b_e+(7940(c&#h*b6m(&$(ckl^84mdl+k+gr_g2a'
+# SECRET_KEY='django-insecure-xx8z#nn3t&b_e+(7940(c&#h*b6m(&$(ckl^84mdl+k+gr_g2a'
 
 MODE=config("MODE", default="dev")
 
