e4827fa5f754a8ed6a1567224b00708d094a4ad2
bensammwaniki <bensammwaniki@gmail.com>
2021-10-11 04:51:46 +0300

    displaying images

--- galleryapp/views.py
+++ galleryapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Image, Location, Category
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    dispimages = Image.objects.all().order_by('-id')
+    dispBylocation = Location.objects.all()
+    dispBycategory = Category.objects.all()
+
+    return render(request, "index.html", {'images': dispimages, 'locations': dispBylocation, 'categories': dispBycategory})
